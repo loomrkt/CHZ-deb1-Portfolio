3f94aa8918626566f2968649042b5db20387590b
RAKOTONDRAZAKA Nameno Fanantenana <118853977+loomrkt@users.noreply.github.com>
2024-01-14 23:07:13 +0300

    Update mock-hero.ts

--- src/app/pages/unipage/heroSection/mock-hero.ts
+++ src/app/pages/unipage/heroSection/mock-hero.ts
@@ -1,7 +1,7 @@
 import { Hero } from "../../../models/hero.model";
 
 export const HERO: Hero = {
-  description: "Curieuse, déterminée, et toujours avide d'apprendre et de relever de nouveaux défis. Je suis un développeur passionné par la creation de solutions innovantes.",
+  description: "Curieux, déterminé et constamment désireux d'acquérir de nouvelles connaissances tout en relevant de nouveaux défis. Je suis un développeur passionné par la création de solutions innovantes.",
   proffesion: "Developpeur Full-Stack",
   img: "../../../../assets/MyPhoto.jpg"
 }
